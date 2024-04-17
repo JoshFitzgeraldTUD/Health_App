523fbc736a99d6baeabb8fb614f7debeb4a3d1c4
X00188120 Joshua Fitzgerald <x00188120@TA.IT-TALLAGHT.IE>
2024-04-17 14:32:41 +0100

    further refinements

--- patients/urls.py
+++ patients/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from . import views
+from .views import SignUpView
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('login/', views.user_login, name='login'),
-    path('signup/', views.user_signup, name='signup'),
-    path('logout/', views.user_logout, name='logout'),
+    path("signup/", SignUpView.as_view(), name="signup"),
 ]
--- patients/views.py
+++ patients/views.py
@@ -1,39 +1,9 @@
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from .forms import SignupForm, LoginForm
-
-# Create your views here.
-#Home Page
-def index(request):
-    return render(request, 'home.html')
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 
 #Sign up page
-def user_signup(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-    if form.is_valid():
-        form.save()
-        return redirect('login')
-    else:
-        form = SignupForm()
-    return render(request, 'signup.html', {'form': form})
-
-#login page
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)    
-                return redirect('home')
-    else:
-        form = LoginForm()
-    return render(request, 'login.html', {'form': form})
-
-#log out page
-def user_logout(request):
-    logout(request)
-    return redirect('login')
+class SignUpView(CreateView):
+     form_class = UserCreationForm
+     success_url = reverse_lazy("login")
+     template_name = "registration/signup.html"
