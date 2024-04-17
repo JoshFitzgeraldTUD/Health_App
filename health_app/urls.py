a88b8b128ded046e8c1d0be180aa6906b3104f14
X00188120 Joshua Fitzgerald <x00188120@TA.IT-TALLAGHT.IE>
2024-04-17 14:51:54 +0100

    resolving merge conflicts attempt 1

--- health_app/urls.py
+++ health_app/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
 from calander import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
